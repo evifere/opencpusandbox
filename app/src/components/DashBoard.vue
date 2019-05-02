ecf14a75f62a5fdf502916752b3d34ad6ee20e81
JOLLY <evifere@gmail.com>
2019-05-02 20:22:02 +0200

    initial app commit

---
+++ app/src/components/DashBoard.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container>
+    <el-header>
+    Headers
+    </el-header>
+    <el-main>
+      dashboardlab
+    </el-main>
+    <el-footer align="right">copyright 2018-2019 @dashboardlab v{{version}}</el-footer>
+  </el-container>
+</template>
+
+<script scoped>
+import { version } from "../../package.json";
+
+export default {
+  name: "Dashboard",
+  props: {
+    msg: String
+  },
+
+  data() {
+    return {
+     
+      version: version,
+    };
+  }
+};
+</script>
