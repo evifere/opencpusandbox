467b057d2f0995a0eecfb0ee947c7428c3567971
JOLLY <evifere@gmail.com>
2019-05-05 20:32:05 +0200

    factorize code

--- app/src/components/DashBoard.vue
+++ app/src/components/DashBoard.vue
@@ -51,13 +51,13 @@
       <el-main>
         <el-row>
           <el-col :span="8">
-            <highcharts :options="chartOptions"></highcharts>
+            <highcharts :options="chartFruitsOptionsA"></highcharts>
           </el-col>
           <el-col :span="8">
-            <highcharts :options="chartOptionsB"></highcharts>
+            <highcharts :options="chartFruitsOptionsB"></highcharts>
           </el-col>
           <el-col :span="8">
-            <highcharts :options="chartOptions"></highcharts>
+            <highcharts :options="chartFruitsOptionsC"></highcharts>
           </el-col>
         </el-row>
         <el-row>
@@ -114,14 +114,9 @@ export default {
       version: version,
       isCollapse: false,
       activeIndex: "5",
-      chartOptions: {
-        series: [
-          {
-            data: [3, 5, 7] // sample data
-          }
-        ]
-      },
-      chartOptionsB:{},
+      chartFruitsOptionsA: {},
+      chartFruitsOptionsB: {},
+      chartFruitsOptionsC: {},
       randomSeries: [],
       candleStickOptions: require("../config/candlestick"),
       histogramOptions: require("../config/histo"),
@@ -132,102 +127,25 @@ export default {
   },
 
   created() {
-    let _self = this;
-    this.$http
-      .get(
-        process.env.VUE_APP_API_BASE_URI +
-          "ocpu/user/opencpu/library/mysamplepack/R/test/json?auto_unbox=true"
-      )
-      .then(response => {
-        this.chartOptions = response.data;
-      });
-
-    this.$http
-      .post(
-        process.env.VUE_APP_API_BASE_URI +
-          "/ocpu/user/opencpu/library/chartreader/R/randomplot2",
-        {
-          n: 1000,
-          dist: "normal"
-        }
-      )
-      .then(response => {
-        let resultUrl =
-          process.env.VUE_APP_API_BASE_URI +
-          response.data.split("\n")[0] +
-          "/json?auto_unbox=true";
-
-        _self.$http.get(resultUrl).then(response => {
-          this.histogramOptions.series[0].data = response.data.counts;
-          this.histogramOptions.series[0].breaks = response.data.breaks;
-        });
-      });
-
-    this.$http
-      .post(
-        process.env.VUE_APP_API_BASE_URI +
-          "/ocpu/user/opencpu/library/chartreader/R/randomplot2",
-        {
-          n: 1000,
-          dist: "uniform"
-        }
-      )
-      .then(response => {
-        let resultUrl =
-          process.env.VUE_APP_API_BASE_URI +
-          response.data.split("\n")[0] +
-          "/json?auto_unbox=true";
-        _self.$http.get(resultUrl).then(response => {
-          this.histogramUniformOptions.series[0].data = response.data.counts;
-          this.histogramUniformOptions.series[0].breaks = response.data.breaks;
-        });
-      });
-
-    this.$http
-      .post(
-        process.env.VUE_APP_API_BASE_URI +
-          "/ocpu/user/opencpu/library/chartreader/R/randompie",
-        {
-          n: 10
-        }
-      )
-      .then(response => {
-        let resultUrl =
-          process.env.VUE_APP_API_BASE_URI +
-          response.data.split("\n")[0] +
-          "/json";
-        _self.$http.get(resultUrl).then(response => {
-          this.chartPieOptions.series[0].data = response.data.series.map(
-            (el, i) => {
-              return { y: el, name: "pie n = " + i };
-            }
-          );
-        });
-      });
-
-    this.$http
-      .post(
-        process.env.VUE_APP_API_BASE_URI +
-          "/ocpu/user/opencpu/library/chartreader/R/randomfruits",
-        {
-          n: 3
-        }
-      )
-      .then(response => {
-        let resultUrl =
-          process.env.VUE_APP_API_BASE_URI +
-          response.data.split("\n")[0] +
-          "/json?auto_unbox=true";
-        _self.$http.get(resultUrl).then(response => {
-          console.log(response.data);
-          _self.chartOptionsB = response.data;
-        });
-      });
-    this.$http
-      .get(process.env.VUE_APP_BASE_URI + "aapl-ohlc.json")
-      .then(response => {
-        this.candleStickOptions.series[0].data = response.data;
-      });
+    this.loadRandomplots(
+      {
+        n: 1000,
+        dist: "normal"
+      },
+      "histogramOptions"
+    );
+    this.loadRandomplots(
+      {
+        n: 1000,
+        dist: "uniform"
+      },
+      "histogramUniformOptions"
+    );
+    this.loadRandompie();
+    this.loadRandomFruits("chartFruitsOptionsA");
+    this.loadRandomFruits("chartFruitsOptionsB");
+    this.loadRandomFruits("chartFruitsOptionsC");
+    this.loadCandlestick();
   },
 
   methods: {
@@ -236,6 +154,80 @@ export default {
     },
     handleSelect() {
       console.log("handleSelect");
+    },
+    loadRandomFruits(optionKeyname) {
+      let _self = this;
+
+      this.$http
+        .post(
+          process.env.VUE_APP_API_BASE_URI +
+            "/ocpu/user/opencpu/library/chartreader/R/randomfruits",
+          {
+            n: 3
+          }
+        )
+        .then(response => {
+          let resultUrl =
+            process.env.VUE_APP_API_BASE_URI +
+            response.data.split("\n")[0] +
+            "/json?auto_unbox=true";
+          _self.$http.get(resultUrl).then(response => {
+            console.log(optionKeyname, response.data);
+            _self[optionKeyname] = response.data;
+          });
+        });
+    },
+    loadCandlestick() {
+      this.$http
+        .get(process.env.VUE_APP_BASE_URI + "aapl-ohlc.json")
+        .then(response => {
+          this.candleStickOptions.series[0].data = response.data;
+        });
+    },
+    loadRandompie() {
+      let _self = this;
+
+      this.$http
+        .post(
+          process.env.VUE_APP_API_BASE_URI +
+            "/ocpu/user/opencpu/library/chartreader/R/randompie",
+          {
+            n: 10
+          }
+        )
+        .then(response => {
+          let resultUrl =
+            process.env.VUE_APP_API_BASE_URI +
+            response.data.split("\n")[0] +
+            "/json";
+          _self.$http.get(resultUrl).then(response => {
+            this.chartPieOptions.series[0].data = response.data.series.map(
+              (el, i) => {
+                return { y: el, name: "pie n = " + i };
+              }
+            );
+          });
+        });
+    },
+    loadRandomplots(data, key) {
+      let _self = this;
+
+      this.$http
+        .post(
+          process.env.VUE_APP_API_BASE_URI +
+            "/ocpu/user/opencpu/library/chartreader/R/randomplot2",
+          data
+        )
+        .then(response => {
+          let resultUrl =
+            process.env.VUE_APP_API_BASE_URI +
+            response.data.split("\n")[0] +
+            "/json?auto_unbox=true";
+          _self.$http.get(resultUrl).then(response => {
+            this[key].series[0].data = response.data.counts;
+            this[key].series[0].breaks = response.data.breaks;
+          });
+        });
     }
   }
 };
