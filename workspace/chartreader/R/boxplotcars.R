8a762a0e3a10360dc0009ac0ef8d679a95b70caa
JOLLY <evifere@gmail.com>
2019-05-08 14:28:12 +0200

    add some comment

--- workspace/chartreader/R/boxplotcars.R
+++ workspace/chartreader/R/boxplotcars.R
@@ -1,5 +1,14 @@
 # Plot the chart.
+# stats
 
+#stats a vector of length 5, containing : , , the median, and the extreme of the upper whisker.
+# see https://www.statisticshowto.datasciencecentral.com/upper-hinge-lower-hinge
+# https://towardsdatascience.com/understanding-boxplots-5e2df7bcbd51?gi=312ea7c43453
+# the extreme of the lower whisker
+# the lower ‘hinge’ Q1
+# the median       Q2
+# the upper ‘hinge’  Q3
+# the upper whisker
 boxplotcars <- function(){
 result <-boxplot(mpg ~ cyl, data = mtcars,
                xlab = "Number of Cylinders",
@@ -10,6 +19,9 @@ result <-boxplot(mpg ~ cyl, data = mtcars,
                col = c("green","yellow","purple"),
                names = c("High","Medium","Low")
 )
+
+result$summary = unclass(summary(result$stats))
+
 
 result
 }
