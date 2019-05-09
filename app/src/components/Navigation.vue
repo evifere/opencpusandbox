441d4ab6a792db630ff920b41f2e66ea41c1dea1
JOLLY <evifere@gmail.com>
2019-05-09 18:50:29 +0200

    update current index nav and side bar

--- app/src/components/Navigation.vue
+++ app/src/components/Navigation.vue
@@ -1,50 +1,55 @@
 <template>
-    <el-header>
-      <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        @select="handleSelect"
-      >
-        <el-menu-item index="1">Processing Center</el-menu-item>
-        <el-menu-item index="4">Analyse Center</el-menu-item>
-        <el-submenu index="2">
-          <template slot="title">Workspace</template>
-          <el-menu-item index="2-1">Yanone</el-menu-item>
-          <el-menu-item index="2-2">Milktalis</el-menu-item>
-          <el-menu-item index="2-3">Miam miam burger</el-menu-item>
-        </el-submenu>
-        <el-menu-item index="5"  @click="onDashboard()">Dashboard</el-menu-item>
-        <el-submenu index="6" class="right-menu-item">
-          <template slot="title">
-            <img :src="'icons/female-profile.png'" width="48" height="48">User Account
-          </template>
-          <el-menu-item index="6-1" @click="onLogout()">Logout</el-menu-item>
-          <el-menu-item index="6-2">Settings</el-menu-item>
-        </el-submenu>
-      </el-menu>
-    </el-header>
+  <el-header>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">Processing Center</el-menu-item>
+      <el-menu-item index="4">Analyse Center</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">Workspace</template>
+        <el-menu-item index="2-1">Yanone</el-menu-item>
+        <el-menu-item index="2-2">Milktalis</el-menu-item>
+        <el-menu-item index="2-3">Miam miam burger</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="5" @click="onDashboard()">Dashboard</el-menu-item>
+      <el-submenu index="6" class="right-menu-item">
+        <template slot="title">
+          <img :src="'icons/female-profile.png'" width="48" height="48">User Account
+        </template>
+        <el-menu-item index="6-1" @click="onLogout()">Logout</el-menu-item>
+        <el-menu-item index="6-2">Settings</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </el-header>
 </template>
 
 
 <script scoped>
-
 export default {
   name: "Navigation",
 
-  data() {
-    return {
-      activeIndex: "5",
-    };
+  computed: {
+    activeIndex: {
+      get() {
+        return localStorage.getItem("activeIndex") || "5";
+      },
+      set(value) {
+        localStorage.setItem("activeIndex", value);
+      }
+    }
   },
 
   methods: {
     handleSelect(key) {
-      console.log("handleSelect",key);
+      console.log("handleSelect", key);
+      this.activeIndex = key;
     },
-  
-    onDashboard(){
-      this.$router.push({ path: '/dashboard' })
+
+    onDashboard() {
+      this.$router.push({ path: "/dashboard" });
     },
     onLogout() {
       this.isConnected = false;
--- app/src/components/Sidebar.vue
+++ app/src/components/Sidebar.vue
@@ -1,26 +1,31 @@
  <template>
   <el-aside width="128px">
-        <el-menu class="el-menu-vertical-demo" @select="handleVerticalMenu" :collapse="isCollapse">
-          <el-menu-item index="candlestick">
-            <img :src="'icons/candlestick.png'" width="48" height="48">
-          </el-menu-item>
-          <el-menu-item index="combo-chart">
-            <img :src="'icons/combo-chart.png'" width="48" height="48">
-          </el-menu-item>
-          <el-menu-item index="line-chart">
-            <img :src="'icons/line-chart.png'" width="48" height="48">
-          </el-menu-item>
-          <el-menu-item index="pie-chart">
-            <img :src="'icons/pie-chart.png'" width="48" height="48">
-          </el-menu-item>
-          <el-menu-item index="histogram">
-            <img :src="'icons/histogram.png'" width="48" height="48">
-          </el-menu-item>
-          <el-menu-item index="line-graphic">
-            <img :src="'icons/line-graphic.png'" width="48" height="48">
-          </el-menu-item>
-        </el-menu>
-      </el-aside>
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-vertical-demo"
+      @select="handleVerticalMenu"
+      :collapse="isCollapse"
+    >
+      <el-menu-item index="candlestick">
+        <img :src="'icons/candlestick.png'" width="48" height="48">
+      </el-menu-item>
+      <el-menu-item index="combo-chart">
+        <img :src="'icons/combo-chart.png'" width="48" height="48">
+      </el-menu-item>
+      <el-menu-item index="line-chart">
+        <img :src="'icons/line-chart.png'" width="48" height="48">
+      </el-menu-item>
+      <el-menu-item index="pie-chart">
+        <img :src="'icons/pie-chart.png'" width="48" height="48">
+      </el-menu-item>
+      <el-menu-item index="histogram">
+        <img :src="'icons/histogram.png'" width="48" height="48">
+      </el-menu-item>
+      <el-menu-item index="line-graphic">
+        <img :src="'icons/line-graphic.png'" width="48" height="48">
+      </el-menu-item>
+    </el-menu>
+  </el-aside>
 </template>
 <script scoped>
 export default {
@@ -28,17 +33,24 @@ export default {
 
   data() {
     return {
-      isCollapse: false,
+      isCollapse: false
     };
   },
 
-
+  computed: {
+    activeIndex: {
+      get() {
+        return localStorage.getItem("activeIndex");
+      },
+      set(value) {
+        localStorage.setItem("activeIndex", value);
+      }
+    }
+  },
   methods: {
     handleVerticalMenu(key, keyPath) {
-      console.log("handleVerticalMenu", key, keyPath);
-      console.log("handleVerticalMenu", this.$route);
-     
-     this.$router.push({ path: '/'+key })
+      this.activeIndex = key;
+      this.$router.push({ path: "/" + key });
     }
   }
 };
@@ -49,7 +61,9 @@ export default {
   min-height: 53em;
 }
 
-.el-menu--horizontal > .el-submenu.right-menu-item {
-  float: right;
+.el-menu-item.is-active img {
+  border-radius: 5px 20px 5px;
+  padding: 0.2em;
+  background-color: rgb(31, 189, 176);
 }
 </style>
